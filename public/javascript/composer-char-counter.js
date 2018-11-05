7613c174c54fcc2fe47787e2a6a7addf588d6de3
Serhii Panchyshyn <panchyshyn.serhii@gmail.com>
2018-11-05 23:40:45 +0000

    added jqeury file to count chars in the textarea

---
+++ public/javascript/composer-char-counter.js
@@ -0,0 +1,21 @@
+$(document).ready(() => {
+  console.log('Ready to go!');
+  $('textarea').on('input', function() {
+    const elementLength = $(this).val().length;
+    const counter = $(this)
+      .parent()
+      .find('.counter');
+    const btn = $(this)
+      .parent()
+      .find('.btn-send');
+    const maxLenght = 140;
+    const char = maxLenght - elementLength;
+    if (elementLength >= maxLenght) {
+      counter.html(`${char} you have reached the limit`).addClass('text-msg');
+      btn.prop('disabled', true);
+    } else {
+      counter.html(`${char} characters left`).removeClass('text-msg');
+      btn.prop('disabled', false);
+    }
+  });
+});
