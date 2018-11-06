29bd6176809cb0e5a31ea352ba07984728b85928
Serhii Panchyshyn <panchyshyn.serhii@gmail.com>
2018-11-06 00:57:26 +0000

    finished user profie

---
+++ public/javascript/app.js
@@ -0,0 +1,27 @@
+$(document).ready(() => {
+  // Test / driver code (temporary). Eventually will get this from the server.
+  const tweetData = {
+    user: {
+      name: 'Newton',
+      avatars: {
+        small: 'https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png',
+        regular: 'https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png',
+        large:
+          'https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png',
+      },
+      handle: '@SirIsaac',
+    },
+    content: {
+      text:
+        'If I have seen further it is by standing on the shoulders of giants',
+    },
+    created_at: 1461116232227,
+  };
+  const createTweetElement = tweet => {};
+
+  const $tweet = createTweetElement(tweetData);
+
+  // Test / driver code (temporary)
+  console.log($tweet); // to see what it looks like
+  $('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+});
